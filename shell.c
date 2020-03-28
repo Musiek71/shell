a3066114b803d36f2065ffa9c2947c820fe8a733
Musiek71 <Mzjedz@gmail.com>
2020-03-28 23:41:39 +0100

    basic commands

--- shell.c
+++ shell.c
@@ -2,13 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
+#include <sys/wait.h>
 
-#define BUF_SIZE 1024
+#define BUF_SIZE 128
 #define RED "\033[0;31m"
 #define RESET "\e[0m"
 
+//TODO make use of chdir (internal cmd)
+//TODO internal commands
+//TODO execs
+
 char *read_line();
 char **parse_line(char *line, int *counter);
+int shell_execute(char **args);
+int shell_exit(char **args);
+void print_tokens(char **tokens, int counter);
+int check_internal_commands(char **args);
+
 
 
 void loop(){
@@ -17,20 +28,18 @@ void loop(){
   int counter;
   int status = 1;
 
-  while (status){
-    fprintf(stdout, ">: ");
+  do {
+    printf(">: ");
     line = read_line();
     tokens = parse_line(line, &counter);
 
-    printf("%d\n", counter);
-    for (int i = 0; i < counter; i++){
-      printf("%s\n", tokens[i]);
-    }
+    //print_tokens(tokens, counter);
 
+    status = shell_execute(tokens);
 
     free(line);
     free(tokens);
-  }
+  } while (status);
 }
 
 int main(){
@@ -105,4 +114,67 @@ char **parse_line(char *line, int *counter){
 
   return tokens;
 
+}
+
+int shell_execute(char **args){
+  pid_t cpid;
+  int process_status;
+  int flag;
+
+  flag = check_internal_commands(args);
+  switch (flag){
+    case (-1): return 0;
+    case (1): return 1;
+  }
+
+
+  cpid = fork();
+
+  if (cpid == 0) {
+    if (execvp(args[0], args) < 0)
+      printf("command not found: \"%s\"\n", args[0]);
+    exit(EXIT_FAILURE);
+
+  } else if (cpid < 0)
+    printf(RED "Error forking"
+      RESET "\n");
+  else {
+    waitpid(cpid, &process_status, WUNTRACED);
+  }
+  return 1;
+}
+
+int shell_exit(char **args){
+  return -1;
+}
+
+void print_tokens(char **tokens, int counter){
+    printf("command count:%d\n", counter);
+    for (int i = 0; i < counter; i++){
+      printf("%s\n", tokens[i]);
+    }
+}
+
+int check_internal_commands(char **args){
+  char tmp[128];
+
+  if (strcmp(args[0], "exit") == 0) {
+    return shell_exit(args);
+  } 
+  else if (strcmp(args[0], "cwd") == 0){
+    printf("%s\n", getcwd(tmp, 128));
+    return 1;
+  } 
+  else if (strcmp(args[0], "cd") == 0){
+    if (chdir(args[1]) != 0){
+      fprintf(stderr, "Failed to chdir to \"%s\"\n", args[1]);
+      //TODO include errno
+    }
+    printf("%s\n", getcwd(tmp, 128));
+    return 1;
+  }
+  else if (strcmp(args[0], "xD") == 0){
+    printf("xD\n");
+    return 1;
+  } 
 }
